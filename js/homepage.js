a7ac019fd7b59e5a76d1f841981d97db05876028
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2019-03-04 21:02:34 -0500

    Added simple CMS with google sheets

---
+++ js/homepage.js
@@ -0,0 +1,81 @@
+
+$(async () => {
+    const sheetId = '1YdWKrR8FgzwaQg18lvegrn_2ZKzBERi-jUPFWgCFI4Y';
+
+    $.ajax({
+
+        url : `https://spreadsheets.google.com/feeds/cells/${sheetId}/1/public/full?alt=json`,
+        type : 'GET',
+        dataType:'json',
+        success : function(result) {              
+            const sheet = getSheet(result);
+            console.log(sheet);
+            updatePageWithData(sheet);
+        },
+        error : function(request,error) {
+            alert('Failed to load website. Please try again');
+        }
+    });
+});
+
+function getSheet(result) {
+    const sheetEntity = {};
+    result.feed.entry.forEach(entry => {
+        sheetEntity[entry.title.$t] = entry.content.$t;
+    });
+
+    return sheetEntity;
+}
+
+function updatePageWithData(sheetData) {
+    // Header 
+    $('#header-about').text(sheetData.A3);
+    $('#header-portfolio').text(sheetData.B3);
+    $('#header-pricing').text(sheetData.C3);
+    $('#header-contact').text(sheetData.D3);
+
+    // Carousel
+    addCarouselImages(sheetData);
+
+    // Links 
+    $('#link-1').text(sheetData.A11);
+    $('#link-2').text(sheetData.B11);
+
+    // Social
+    $('#social-email').attr('href', `mailto:${sheetData.A15}`);
+    $('#social-instagram').attr('href', sheetData.A19);
+    $('#social-facebook').attr('href', sheetData.B19);
+
+    // Footer
+    $('#email').text(sheetData.A15);
+    $('#copyright-text').text(sheetData.B15);
+    $('#copyright-date').text(new Date().getFullYear());
+
+    // End
+    $('.logo-loader').fadeOut(1000);
+}
+
+function addCarouselImages(sheetData) {
+    const keys = Object.keys(sheetData).filter(key => key.includes('7') && !key.includes('1') && !key.includes('2'));
+    keys.forEach((cell, index) => {
+        $('#main-carousel').append(generateCarouselImage(sheetData[cell], index));
+        $('#main-carousel-indicators').append(generateCarouselIndicators(sheetData[cell]));
+    })
+
+    $('#carouselExampleIndicators').carousel();
+}
+
+function generateCarouselImage(imgUrl, index) {
+    console.log(imgUrl);
+    return `
+    <div class="carousel-item ${!index ? 'active' : ''}">
+    <img src="${imgUrl}" class="d-block w-100" alt="carousel image">
+    </div>`;
+}
+
+function generateCarouselIndicators(index) {
+    const html = `
+    <li data-target="#carouselExampleIndicators" data-slide-to="${index}" ${!index ? 'class="active"' : ''}></li>`;
+
+    return html;
+}
